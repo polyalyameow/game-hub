832f9315833f00235338171218e7b461eb5872ac
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2023-07-06 23:36:04 +0200

    Refactoring Game Grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,9 +15,9 @@ const GameGrid = ({gameQuery}:Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
+    
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
@@ -35,7 +35,7 @@ const GameGrid = ({gameQuery}:Props) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
+ 
   );
 };
 
